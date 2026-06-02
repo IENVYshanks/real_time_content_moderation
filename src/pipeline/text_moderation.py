dfc25064c88668a6997965cc25dff797e447d0fb
IENVYshanks <sawaiyanronit12@gmail.com>
2026-06-02 06:17:38 +0530

    small changes

--- src/pipeline/text_moderation.py
+++ src/pipeline/text_moderation.py
@@ -1,8 +1,7 @@
 from src.models.user_text import UserText
 from src.utils.regex import moderate
 
-def text_pipeline(text: UserText) -> bool:
-    if(moderate(text.text)){
-        
-    }
-    return False
+def text_pipeline(text: str) -> bool:
+    if(moderate(text)):
+        pass
+    return True
--- src/router/text_ingesation.py
+++ src/router/text_ingesation.py
@@ -4,6 +4,6 @@ from src.pipeline.text_moderation import text_pipeline
 router = APIRouter(prefix = "/text_ingestion", tags=["text_ingestion"])
 
 @router.post("/text_ingest")
-async def ingest_text(text : UserText ):
-    is_moderate =text_pipeline(text)
+async def ingest_text(text : UserText):
+    is_moderate =text_pipeline(text.text)
     return {"message": text, "is_moderate": is_moderate}
